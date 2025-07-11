340f5c8b01f263151ebdf68beb38670da74acc0d
LaisRochaS <laismaria713@gmail.com>
2025-07-11 11:30:43 -0400

    pass 5

--- controllers/users.js
+++ controllers/users.js
@@ -1,40 +1,21 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-// GET all users
 const getAll = async (req, res) => {
-    try {
-        const result = await mongodb.getDatabase().db().collection('contacts').find();
-        const users = await result.toArray();
+    const result = await mongodb.getDatabase().db().collection('contacts').find();
+    result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to get users' });
-    }
+    });
 };
 
-// GET a single user by ID
 const getSingle = async (req, res) => {
-    const userId = req.params.id;
-
-    // Validate ObjectId format
-    if (!ObjectId.isValid(userId)) {
-        return res.status(400).json({ error: 'Invalid user ID format' });
-    }
-
-    try {
-        const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: new ObjectId(userId) });
-        const users = await result.toArray();
-
-        if (!users.length) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId });
+    result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to get user' });
-    }
+        res.status(200).json(users[0]);
+    });
 };
 
 module.exports = {
